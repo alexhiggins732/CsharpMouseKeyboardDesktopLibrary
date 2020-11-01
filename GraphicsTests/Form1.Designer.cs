70086d0502a7b54dc83b8fba4aaa39bf06415c3b
Alexander Higgins <alexhiggins732@hotmail.com>
2020-11-01 18:02:41 -0500

    Graphics Test

---
+++ GraphicsTests/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace GraphicsTests
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.processList = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.processList.FormattingEnabled = true;
+            this.processList.Location = new System.Drawing.Point(12, 12);
+            this.processList.Name = "comboBox1";
+            this.processList.Size = new System.Drawing.Size(300, 21);
+            this.processList.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 389);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.processList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox processList;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ GraphicsTests/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsTests
+{
+    public partial class Form1 : Form
+    {
+        
+        dynamic GetDc()
+        {
+            var bmp = new Bitmap(1, 1);
+            var g = Graphics.FromImage(bmp);
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(x => x.FullName.Contains("Drawing")).ToList();
+            var type = assemblies.SelectMany(x => x.DefinedTypes.Where(t => t.FullName == "System.Drawing.Internal.DeviceContext")).First();
+
+            var contextTypes = assemblies.SelectMany(x => x.DefinedTypes.Where(t => t.FullName == "System.Drawing.Internal.DeviceContexts")).First();
+            var contextsField = contextTypes.GetField("activeDeviceContexts", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+            var contexts = contextsField.GetValue(null);
+
+
+            var frm = this;
+            var hwnd = frm.Handle;
+            var m = type.GetMethod("FromHwnd");
+            var m2 = type.GetField("hCurrentBmp", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+         
+       
+            dynamic result = m.Invoke(null, new object[] { hwnd });
+            var Hdc = result.Hdc;
+            var bmpHwnd = m2.GetValue(result);
+            var ip = new IntPtr(0x0005004a);
+            var bm2 = Bitmap.FromHbitmap(bmpHwnd);
+            this.pictureBox1.Image = bm2;
+            return result;
+
+
+
+
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            GetDc();
+            //processList.SelectedValueChanged += ProcessList_SelectedValueChanged;
+            //bindProcesses();
+
+        }
+
+
+        private void bindProcesses()
+        {
+
+            this.processList.Items.Clear();
+            processList.Items.Add(new ProcessInfo());
+            foreach (var process in Process.GetProcesses().OrderBy(x => x.ProcessName))
+            {
+
+                try
+                {
+                    var hnd = process.Handle;
+                    this.processList.Items.Add(new ProcessInfo(process));
+                }
+                catch { }
+            }
+
+            //processList.DisplayMember = "Name";
+            var currentId = Process.GetCurrentProcess().Id;
+            this.pictureBox1.Image = new Bitmap(1024, 768);
+            this.DrawToBitmap((Bitmap)this.pictureBox1.Image, new Rectangle(0, 0, 1024, 768));
+            foreach (ProcessInfo processInfo in this.processList.Items)
+            {
+                if (processInfo.Id == currentId)
+                {
+                    // processList.SelectedItem = processInfo;
+                }
+            }
+        }
+
+        private void ProcessList_SelectedValueChanged(object sender, EventArgs e)
+        {
+            ProcessInfo processInfo = (ProcessInfo)this.processList.SelectedItem;
+            if (processInfo != null)
+            {
+                try
+                {
+                    var handle = processInfo.Process?.Handle;
+                    if (handle != null)
+                    {
+                        var source = Bitmap.FromHbitmap(handle.Value);
+                    
+                        using (var g = this.pictureBox1.CreateGraphics())
+                        {
+                          
+                            g.DrawImage(source, 0, 0);
+                        }
+                    }
+                }
+                catch (Exception ex) 
+                {
+                    Console.WriteLine(ex.Message);
+
+                }
+
+
+            }
+        }
+    }
+
+    public class ProcessInfo
+    {
+        public Process Process { get; private set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public ProcessInfo()
+        {
+
+        }
+        public ProcessInfo(Process process)
+        {
+            this.Process = process;
+            this.Id = process.Id;
+            this.Name = process.ProcessName;
+        }
+        public override string ToString()
+        {
+            return Process == null ? "" : $"{Id} - {Name}";
+        }
+    }
+}
