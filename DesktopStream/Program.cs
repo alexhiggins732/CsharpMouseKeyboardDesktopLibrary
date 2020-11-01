15f556a593736688b5c90635df6cb4ddd797a490
Alexander Higgins <alexhiggins732@hotmail.com>
2020-11-01 18:57:44 -0500

    Revent screen capture code

--- DesktopStream/Program.cs
+++ DesktopStream/Program.cs
@@ -35,39 +35,9 @@ namespace DesktopStream
             }
             throw new Exception("No network adapters with an IPv4 address in the system!");
         }
-        static dynamic GetContext()
-        {
-            var bmp = new Bitmap(5, 5);
-            var g = Graphics.FromImage(bmp);
-            var frm = new Form();
-            frm.Show();
-            frm.DrawToBitmap(bmp, new Rectangle(0, 0, 5, 5));
-            var Assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(x => x.FullName.Contains("Drawing")).ToList();
-            var type = Assemblies.SelectMany(x => x.DefinedTypes.Where(t => t.FullName == "System.Drawing.Internal.DeviceContext")).First();
 
-            var types = Assemblies.SelectMany(x => x.DefinedTypes.Where(t => t.FullName == "System.Drawing.Internal.DeviceContexts")).First();
-            var contextsField = types.GetField("activeDeviceContexts", BindingFlags.Static | BindingFlags.NonPublic);
-            var contexts = contextsField.GetValue(null);
-
-
-            var hwnd = frm.Handle;
-            var m = type.GetMethod("FromHwnd");
-            var mCurrentBmp = type.GetField("hCurrentBmp", BindingFlags.Instance | BindingFlags.NonPublic);
-            var dc = type.GetField("hDC", BindingFlags.Instance | BindingFlags.NonPublic);
-            dynamic result = m.Invoke(null, new object[] { hwnd });
-            var bmpHwnd = mCurrentBmp.GetValue(result);
-            var hDC = dc.GetValue(result);
-            frm.Close();
-            ScreenShotUtility.deviceHdc = hDC;
-            return result;
-        }
         static void Main(string[] args)
         {
-            var ctx = GetContext();
-            var p = System.Diagnostics.Process.GetCurrentProcess();
-            var handle = p.Handle;
-            PrivilegeHelper.EnablePrivilege((long)handle, Privileges.SeTcbPrivilege, true);
-
 
             if (args.Length != 0)
             {
@@ -108,7 +78,8 @@ namespace DesktopStream
                 return;
             }
             running = true;
-            Console.WriteLine("The server started successfully, press key 'q' to stop it!");
+            var ep = appServer.Listeners.First().EndPoint;
+            Console.WriteLine($"The server started successfully {ep.Address}:{ep.Port}, press key 'q' to stop it!");
 
             while (Console.ReadKey().KeyChar != 'q')
             {
@@ -151,7 +122,7 @@ namespace DesktopStream
                 var diff = DateTime.Now.Subtract(start).TotalMilliseconds;
                 var sleep = Math.Max(0, fps - diff);
                 System.Threading.Thread.Sleep((int)sleep);
-                session.Close();
+                //session.Close();
             }
 
         }
@@ -185,69 +156,7 @@ namespace DesktopStream
             this.CaptureBuffer = new byte[ScaledWidth * ScaledHeight * 4];
             this.ScaledBuffer = new byte[OriginalWidth * OriginalHeight * 4];
         }
-
-        public Byte[] TakeScreenShot(bool captureMouse = true)
-        {
-            using (var BMP = new Bitmap(ScaledWidth, ScaledHeight))
-            {
-                using (var g = Graphics.FromImage(BMP))
-                {
-                    g.CopyFromScreen(Point.Empty, Point.Empty, new Size(ScaledWidth, ScaledHeight), CopyPixelOperation.SourceCopy);
-
-                    g.Flush();
-
-                    if (Scale == 1)
-                    {
-                        if (captureMouse)
-                        {
-                            CURSORINFO pci;
-                            pci.cbSize = System.Runtime.InteropServices.Marshal.SizeOf(typeof(CURSORINFO));
-
-                            if (GetCursorInfo(out pci))
-                            {
-                                if (pci.flags == CURSOR_SHOWING)
-                                {
-                                    DrawIcon(g.GetHdc(), (int)(pci.ptScreenPos.x), (int)(pci.ptScreenPos.y), pci.hCursor);
-                                    g.ReleaseHdc();
-                                }
-                            }
-                        }
-                        var bits = BMP.LockBits(new Rectangle(0, 0, ScaledWidth, ScaledWidth), ImageLockMode.ReadOnly, PixelFormat.Format32bppRgb);
-                        Marshal.Copy(bits.Scan0, CaptureBuffer, 0, CaptureBuffer.Length);
-                        BMP.UnlockBits(bits);
-                        return CaptureBuffer;
-                    }
-                    else
-                    {
-                        using (var bmp = new Bitmap(BMP, OriginalWidth, OriginalHeight))
-                        {
-                            using (var g2 = Graphics.FromImage(bmp))
-                            {
-                                if (captureMouse)
-                                {
-                                    CURSORINFO pci;
-                                    pci.cbSize = System.Runtime.InteropServices.Marshal.SizeOf(typeof(CURSORINFO));
-
-                                    if (GetCursorInfo(out pci))
-                                    {
-                                        if (pci.flags == CURSOR_SHOWING)
-                                        {
-                                            DrawIcon(g2.GetHdc(), (int)(pci.ptScreenPos.x), (int)(pci.ptScreenPos.y), pci.hCursor);
-                                            g2.ReleaseHdc();
-                                        }
-                                    }
-                                }
-                                var bits = bmp.LockBits(new Rectangle(0, 0, OriginalWidth, OriginalHeight), ImageLockMode.ReadOnly, PixelFormat.Format32bppRgb);
-                                Marshal.Copy(bits.Scan0, ScaledBuffer, 0, ScaledBuffer.Length);
-                                bmp.UnlockBits(bits);
-                                return ScaledBuffer;
-                            }
-                        }
-                    }
-                }
-
-            }
-        }
+   
         [StructLayout(LayoutKind.Sequential)]
         struct CURSORINFO
         {
@@ -315,60 +224,17 @@ namespace DesktopStream
 
             using (var ms = new MemoryStream())
             {
-                var frm = new Form();
-                var screen = Screen.FromControl(frm);
-                frm.Show();
                 using (Graphics g = Graphics.FromImage(bmp))
                 {
-                    //g.CopyFromScreen(Point.Empty, Point.Empty, new Size(scaledWidth, scaledHeight), CopyPixelOperation.SourceCopy);
+                    g.CopyFromScreen(Point.Empty, Point.Empty, new Size(scaledWidth, scaledHeight), CopyPixelOperation.SourceCopy);
                     var dc = IntPtr.Zero;
-                    //System.Drawing.Internal.DeviceContext;
-                    // DeviceContext deviceContext = DeviceContext.FromHdc(IntPtr.Zero);
-                    try
-                    {
-                        //HandleRef screenDC = new HandleRef(null, deviceHdc);
-                        //HandleRef targetDC = new HandleRef(null, g.GetHdc());
-                        try
-                        {
-                            //int result = SafeNativeMethods.BitBlt(targetDC, destinationX, destinationY, destWidth, destHeight,
-                            //                                      screenDC, sourceX, sourceY, unchecked((int)CopyPixelOperation.SourceCopy));
-                            bool result = SafeNativeMethods.BitBlt(g.GetHdc(), destinationX, destinationY, destWidth, destHeight,
-                                                                  deviceHdc, sourceX, sourceY,
-                                                                  (SafeNativeMethods.TernaryRasterOperations)(int)CopyPixelOperation.SourceCopy);
-                            var error = Marshal.GetLastWin32Error();
-                            //a zero result indicates a win32 exception has been thrown
-                            if (result == false)
-                            {
-                                throw new Win32Exception(error);
-                               
-                            }
-                        }
-                        catch(Exception ex )
-                        {
-                            Console.WriteLine($"WinApiOperationfailed: {ex.Message}");
-                        }
-                        finally
-                        {
-                            g.ReleaseHdc();
-                        }
-                        // g.CopyFromScreen(Point.Empty, Point.Empty, new Size(scaledWidth, scaledHeight), CopyPixelOperation.SourceCopy);
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"Exception copying from screen {ex}");
-                        throw;
-                    }
-                    finally
-                    {
-                       // g.ReleaseHdc();
-                    }
                     if (Scale != 1)
                     {
                         bmp = new Bitmap(bmp, Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
                     }
 
                 }
-                frm.Close();
+          
                 if (CaptureMouse)
                 {
                     CURSORINFO pci;
