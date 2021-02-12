8230cedf1939b29a22bbe7f20560322f3ba70754
Alexander Higgins <alexhiggins732@hotmail.com>
2021-02-11 19:34:50 -0500

    Handle Capture Failure

--- GlobalMacroRecorder/ScreenRecorder.cs
+++ GlobalMacroRecorder/ScreenRecorder.cs
@@ -183,7 +183,7 @@ namespace Captura
             var buffer = new byte[Params.Width * Params.Height * 4];
             var timeTillNextFrame = TimeSpan.Zero;
             Control.CheckForIllegalCrossThreadCalls = false;
-            
+
             bool creatingNewFile = false;
             while (!closed)
             {
@@ -198,9 +198,12 @@ namespace Captura
                     while (!stopThread.WaitOne(timeTillNextFrame) && !creatingNewFile)
                     {
                         var timestamp = DateTime.Now;
-                        Screenshot(buffer);
+                        bool captured= Screenshot(buffer);
+                        if (captured)
+                        {
+                            timedWriter.WriteFrame(true, buffer, 0, buffer.Length);
+                        }
                         //videoStream.WriteFrame(true, buffer, 0, buffer.Length);
-                        timedWriter.WriteFrame(true, buffer, 0, buffer.Length);
                         timeTillNextFrame = timestamp + frameInterval - DateTime.Now;
                         if (timeTillNextFrame < TimeSpan.Zero)
                             timeTillNextFrame = TimeSpan.Zero;
@@ -353,21 +356,29 @@ namespace Captura
         }
 
 
-        public void Screenshot(byte[] Buffer)
+        public bool Screenshot(byte[] Buffer)
         {
-            using (var BMP = new Bitmap(Params.Width, Params.Height))
+            try
             {
-                using (var g = Graphics.FromImage(BMP))
+                using (var BMP = new Bitmap(Params.Width, Params.Height))
                 {
-                    //g.CopyFromScreen(Point.Empty, Point.Empty, new Size(Params.Width, Params.Height), CopyPixelOperation.SourceCopy);
-                    g.CopyFromScreen(Params.Location, Point.Empty, new Size(Params.Width, Params.Height), CopyPixelOperation.SourceCopy);
-                    g.Flush();
+                    using (var g = Graphics.FromImage(BMP))
+                    {
+                        //g.CopyFromScreen(Point.Empty, Point.Empty, new Size(Params.Width, Params.Height), CopyPixelOperation.SourceCopy);
+                        g.CopyFromScreen(Params.Location, Point.Empty, new Size(Params.Width, Params.Height), CopyPixelOperation.SourceCopy);
+                        g.Flush();
 
-                    var bits = BMP.LockBits(new Rectangle(0, 0, Params.Width, Params.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppRgb);
-                    Marshal.Copy(bits.Scan0, Buffer, 0, Buffer.Length);
-                    BMP.UnlockBits(bits);
+                        var bits = BMP.LockBits(new Rectangle(0, 0, Params.Width, Params.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppRgb);
+                        Marshal.Copy(bits.Scan0, Buffer, 0, Buffer.Length);
+                        BMP.UnlockBits(bits);
+                        return true;
+                    }
                 }
             }
+            catch
+            {
+            }
+            return false;
         }
     }
 }
